645b662545924d8a083fd9f3bcf95c41628d9042
Arshdeep Singh <331arsh@gmail.com>
2022-02-19 14:46:01 -0600

    Recreated the layout using flexbox.

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 window.onload = function(){
 
-var url = "https://api.nasa.gov/planetary/apod?api_key=ou0LJEahLWPl1YE5luSNLtf4PEHPjJklnYpPQIUq";
-var date = "";
-var btn = document.getElementById("btn");
-var inputBtn = document.getElementById("inputBtn");
-var todaysDate = new Date();
-let text = todaysDate.toISOString().slice(0, 10);
-
-document.getElementById("inputDate").value = text;
-api(url);
-inputBtn.addEventListener("click", function(){
-    btn.innerHTML = "Like";
+    let url = "https://api.nasa.gov/planetary/apod?api_key=ou0LJEahLWPl1YE5luSNLtf4PEHPjJklnYpPQIUq";
+    let date = "";
+    const inputBtn = document.getElementById("inputBtn");
+    const todaysDate = new Date();
+    document.getElementById("inputDate").value = todaysDate.toISOString().slice(0, 10);
+    api(url);
+    inputBtn.addEventListener("click", function(){
     date = document.getElementById("inputDate").value;
     url = url + "&date=" + date;
     api(url);
     url = "https://api.nasa.gov/planetary/apod?api_key=ou0LJEahLWPl1YE5luSNLtf4PEHPjJklnYpPQIUq";
-    
 });
+    
 function api(url)
 {
 fetch(url)
 .then(
     function(response){
-        if(response.status != 200){
+        if(response.status !== 200){
             console.log("There was an error " + response.status);
             return;
         }
@@ -41,16 +37,9 @@ fetch(url)
 function editData(data)
 {
     
-
     document.getElementById("image").setAttribute("src",data.url);
     document.getElementById("title").innerHTML = data.title + " - " + data.date;
     document.getElementById("description").innerHTML = data.explanation;
-    
-    btn.addEventListener("click", function(){
-        if(btn.textContent == "Like")
-        btn.innerHTML = "Unlike";
-        else if(btn.textContent == "Unlike")
-        btn.innerHTML = "Like";
-    });
+
 }
 }
